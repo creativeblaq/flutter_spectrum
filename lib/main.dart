0939ef8cca2141fd71581aeedfe8aec3ff400088
creativeblaq <creativeblaq@gmail.com>
2023-10-15 19:38:06 +0200

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_spectrum/enums/enums.dart';
