ce9a8e20039bd6a3f4346cfdb514f88d9ff788a3
creativeblaq <creativeblaq@gmail.com>
2023-11-08 18:37:33 +0200

    .

--- lib/constructors.dart
+++ lib/constructors.dart
@@ -1,14 +1,10 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 
-class Constructors extends HookWidget {
+class Constructors extends StatelessWidget {
   const Constructors({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final areSameSingletons = useState(false);
-    final areSameTest = useState(false);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Constructors'),
